94a2d78dd949986a6a13a8418e5351e102ba6fd9
johnmartinroque <johnmartinroque@gmail.com>
2026-07-20 11:04:00 +0800

    removed logo

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -51,14 +51,6 @@ function Login() {
     <main className="bg-gray-50 px-4 md:px-8 dark:bg-neutral-900">
       <div className="min-h-screen flex flex-col items-center justify-center">
         <div className="max-w-md w-full">
-          <Link to="/">
-            <img
-              src="https://readymadeui.com/logo-alt.svg"
-              alt="logo"
-              className="w-14 min-h-14 mb-8 mx-auto block"
-            />
-          </Link>
-
           <div className="p-6 rounded-lg bg-white border border-slate-300 shadow-xs md:p-8 dark:bg-neutral-800 dark:border-neutral-700">
             <h1 className="text-slate-900 text-center text-3xl font-bold dark:text-slate-50">
               Sign in
